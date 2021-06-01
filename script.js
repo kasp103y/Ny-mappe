46494e61865c4572a88ca80e7c7803dbe8335684
Kasper Nikolajsen Dahl <kasp103y@stud.kea.dk>
2021-06-01 15:16:30 +0200

    forside

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ function sidenVises() {
 
 function toggleMenu() {
   console.log("toggleMenu");
+  this.offsetLeft;
   document.querySelector("#menu").classList.toggle("hidden");
+  document.querySelector("#menu").classList.toggle("unhidden");
+  this.offsetLeft;
 
   //let erSkjult = document.querySelector("#menu").classList.contains(".open_burger");
 
